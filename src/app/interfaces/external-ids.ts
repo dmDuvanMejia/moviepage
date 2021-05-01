a0729a99ab8fdad601954b7d8d8c581e24aad1de
dquiros <dquiros@ospinternational.com>
2021-04-30 19:42:28 -0500

    Auto-generated commit

--- src/app/interfaces/external-ids.ts
+++
@@ -1,7 +0,0 @@
-export interface ExternalIDS {
-  id: string;
-  imdb_id: string;
-  facebook_id: string;
-  instagram_id: string;
-  twitter_id: string;
-}
--- src/app/interfaces/keywords.ts
+++
@@ -1,9 +0,0 @@
-export interface KeyWords {
-  id:       number;
-  keywords: Keyword[];
-}
-
-export interface Keyword {
-  id:   number;
-  name: string;
-}
